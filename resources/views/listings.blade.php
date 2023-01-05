b161e66f207cc2f55a6939796f5ba022aff4d4e0
Abraham <abrahamsisaysis@gmail.com>
2023-01-05 22:45:40 +0300

    blade comonents introduced

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -9,39 +9,10 @@
             @foreach ($listings as $listing)
                 {{-- <h2> <a href="/listing/{{ $listing['id'] }}"> {{ $listing['title'] }} </a> </h2>
                 <p> {{ $listing['desc'] }} </p> --}}
-                <!-- Item 1 -->
-                <div class="bg-gray-50 border border-gray-200 rounded p-6">
-                    <div class="flex">
-                        <img class="hidden w-48 mr-6 md:block" src="{{ asset('images/acme.png') }}" alt="" />
-                        <div>
-                            <h3 class="text-2xl">
-                                <a href="/listing/{{ $listing->id }}">{{ $listing->title }}</a>
-                            </h3>
-                            <div class="text-xl font-bold mb-4">Acme Corp</div>
-                            <ul class="flex">
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Laravel</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">API</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Backend</a>
-                                </li>
-                                <li
-                                    class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs">
-                                    <a href="#">Vue</a>
-                                </li>
-                            </ul>
-                            <div class="text-lg mt-4">
-                                <i class="fa-solid fa-location-dot"></i> {{ $listing->location }}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+
+                {{-- get listing-card component and pass listing for the prop --}}
+                {{-- <x-listing-card :listing='$listing' /> --}}
+                <x-listing-card :listing="$listing" />
             @endforeach
         @endif
 
