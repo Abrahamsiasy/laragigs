59032e2c68ddc7ea80cdd7484e246eb48b542139
Abraham <abrahamsisaysis@gmail.com>
2023-01-07 20:41:12 +0300

    flush message timer added, alpine js library

--- resources/views/components/flash-messsage.blade.php
+++ resources/views/components/flash-messsage.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('message'))
-    <div class="fixed top-0 transform left-1/2 bg-black -translate-x-1/2 text-white px-48 py-3">
+    <div x-data="{ open: false }" x-init="setTimeOut( () => show = false, 5000)" x-show="open" class="fixed top-0 transform left-1/2 bg-black -translate-x-1/2 text-white px-48 py-3">
         <p> {{ session('message')}}</p>
     </div>
 
